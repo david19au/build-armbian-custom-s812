3e69a26ef1cbc79253f6e7f5a229699e687fc161
SLAzurin <69220163+SLAzurin@users.noreply.github.com>
2021-09-09 14:58:48 -0400

    Add defaults to my testing device

---
+++ compile_s812.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+./compile.sh docker BOARD=aml-s812 BRANCH=current RELEASE=focal BUILD_MINIMAL=no BUILD_DESKTOP=no KERNEL_ONLY=no KERNEL_CONFIGURE=no COMPRESS_OUTPUTIMAGE=sha,gpg,img USE_TORRENT=no
