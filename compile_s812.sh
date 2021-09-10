e9d384e9f8e062ea1254a322bddd1803a24306de
SLAzurin <69220163+SLAzurin@users.noreply.github.com>
2021-09-10 11:15:54 -0400

    Add 150balbes userpatches

--- compile_s812.sh
+++ compile_s812.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-./compile.sh docker BOARD=aml-s812 BRANCH=current RELEASE=focal BUILD_MINIMAL=no BUILD_DESKTOP=no KERNEL_ONLY=no KERNEL_CONFIGURE=no COMPRESS_OUTPUTIMAGE=sha,gpg,img USE_TORRENT=no
+# copy files from userpatches.s8xx to userpatches
+# ./compile.sh docker BOARD=aml-s812 BRANCH=current RELEASE=focal BUILD_MINIMAL=no BUILD_DESKTOP=no KERNEL_ONLY=no KERNEL_CONFIGURE=no COMPRESS_OUTPUTIMAGE=sha,gpg,img USE_TORRENT=no
