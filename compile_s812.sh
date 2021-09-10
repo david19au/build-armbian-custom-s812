5943d96e5311ea8e3cd3fa49a4821c5440c7ee89
SLAzurin <69220163+SLAzurin@users.noreply.github.com>
2021-09-10 11:53:10 -0400

    uncomment compile command

--- compile_s812.sh
+++ compile_s812.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # copy files from userpatches.s8xx to userpatches
-# ./compile.sh docker BOARD=aml-s812 BRANCH=current RELEASE=focal BUILD_MINIMAL=no BUILD_DESKTOP=no KERNEL_ONLY=no KERNEL_CONFIGURE=no COMPRESS_OUTPUTIMAGE=sha,gpg,img USE_TORRENT=no
+./compile.sh docker BOARD=aml-s812 BRANCH=current RELEASE=focal BUILD_MINIMAL=no BUILD_DESKTOP=no KERNEL_ONLY=no KERNEL_CONFIGURE=no COMPRESS_OUTPUTIMAGE=sha,gpg,img USE_TORRENT=no
