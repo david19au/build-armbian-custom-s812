2b7f0d8188f41e87277312ed762ec77463fec760
SLAzurin <69220163+SLAzurin@users.noreply.github.com>
2021-09-10 18:56:07 -0400

    Update postbuild.sh to make it work

--- postbuild.sh
+++ postbuild.sh
@@ -1,9 +1,33 @@
 #!/bin/bash
-# NOT YET AN AUTOMATIC SCRIPT
-# Install kpartx if not already done
-# sudo kpartx -av ./output/images/Armbian_21.11.0-trunk_Aml-s812_focal_current_5.11.0.img
-# Note down loop#, replace that with the loop# below
-# Change LABEL to ROOTFS
-# sudo kpartx -ds /dev/loop28
-# sudo losetup -d /dev/loop28
-# sha256sum $IMAGE > $IMAGE.sha
+# 
+#   This script labels the ext4 partition in a .img file to become ROOTFS
+#   ex: sudo ./postbuild.sh ./output/images/Armbian_*Aml-s812_focal_current_*.img
+
+# Check if script run as sudo/root
+if [[ $(id -u) -ne 0 ]]
+  then echo "Please run as root"
+  exit 1
+fi
+
+# Check if arg0 is file
+if [[ ! -f $1 ]]
+then
+    echo "arg0 not file"
+    exit 1
+fi
+
+# Get free loop partition
+FREE_LOOP=$(losetup -f)
+losetup $FREE_LOOP $1
+if [[ ! $? -eq "0" ]]
+then
+    echo "failed to setup free loop"
+    exit 1
+fi
+
+# Label ext4 partition
+e2label "${FREE_LOOP}p2" ROOTFS
+losetup -d $FREE_LOOP
+
+chown "${USER}:${USER}" $1*
+sha256sum $1 > "${1}.sha"
