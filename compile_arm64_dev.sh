f00d1f7b0baaf6ee43c15a4bc050a4628634635d
Azuri <>
2021-09-14 16:48:51 -0400

    Fix postbuild.sh, no longer needed to rename to ROOTFS

---
+++ compile_arm64_dev.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+./compile.sh docker BOARD=arm-64 BRANCH=current RELEASE=focal BUILD_MINIMAL=no BUILD_DESKTOP=no KERNEL_ONLY=no KERNEL_CONFIGURE=no COMPRESS_OUTPUTIMAGE=sha,gpg,img USE_TORRENT=no CLEAN_LEVEL=""
